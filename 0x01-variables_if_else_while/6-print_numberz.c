1eaa69fbfd0b2624968f4c67963e64ce72fd66e8
Olabeloved <olamakindehabeebah@gmail.com>
2023-03-23 05:09:30 -0700

    Binary

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is for decimal
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
