04404fec0089a14014f58575b3057124fda85135
Olabeloved <olamakindehabeebah@gmail.com>
2023-03-24 14:13:15 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,30 +13,30 @@ int main(void)
 	int k;
 	int l;
 
-		for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
 		{
-			for (j = 0 ; j < 10 ; j++)
+			for (k = 0 ; k < 10 ; k++)
 			{
-				for (k = 0 ; k < 10 ; k++)
+				for (l = 1 ; l < 10 ; l++)
 				{
-					for (l = 1 ; l < 10 ; l++)
+					if (k >= i && i > j)
 					{
-						if (k >= i && i > j)
-						{
-							putchar(i + '0');
-							putchar(j + '0');
-							putchar(' ');
-							putchar(k + '0');
-							putchar(l + '0');
-							if (i + j + k + l != 35)
-							{
-								putchar(',');
-							}
-						}
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i + j + k + l != 35)
+					{
+					putchar(',');
+					}
 					}
 				}
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
