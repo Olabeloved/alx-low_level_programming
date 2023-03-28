9b5f40d2c41a94074f82fecd731c624ef622842e
Olabeloved <olamakindehabeebah@gmail.com>
2023-03-28 15:35:33 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,36 +3,30 @@
 /**
  * times_table - print times table
  *
- * Return: 0
+ * Return: always 0
  */
 
 void times_table(void)
 {
-	int i, j, k, l, m;
+	int i, j, result;
 
 	for (i = 0; i <= 9; i++)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-	k = i + j;
-	if (k > 0)
+	result = i * j;
+	if (result < 10)
 	{
-	l = k % 10;
-	m = (k - l) / 10;
-	_putchar(44);
-	_putchar(12);
-	_putchar(m + '0');
-	_putchar(l + '0');
+	_putchar(result + '0');
 	}
 	else
 	{
-	if (j != 0)
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
+	_putchar(result / 10 + '0');
+	_putchar(result % 10 + '0');
 	}
-	_putchar(k + '0');
+	if (j < 9)
+	{
+	_putchar(',');
 	}
 	}
 	_putchar('\n');
