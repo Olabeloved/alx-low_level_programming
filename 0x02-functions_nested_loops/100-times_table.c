fe97a395215584dd8453b78369b6c742ca31809a
Olabeloved <olamakindehabeebah@gmail.com>
2023-04-03 08:36:19 -0700

    print times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,32 +9,47 @@
 
 void print_times_table(int n)
 {
-	int j, k, result;
+	int j, k, result, remainder;
 
-	if (n < 0 || n > 15)
+	if (n >= 0 && n <= 15)
 	{
 	for (j = 0; j <= n; j++)
 	{
-	_putchar('0');
 	for (k = 0; k <= n; k++)
 	{
-	_putchar(',');
-	_putchar(' ');
-	result = j * k;
-	if (result <= 9)
-	_putchar(' ');
-	if (result <= 99)
-	{
-	_putchar(' ');
-	_putchar(result / 10 + '0');
-	_putchar(result % 10 + '0');
-	}
-	if (result >= 100)
-	{
-	_putchar(result / 100 + '0');
-	_putchar(result / 10 + '0');
-	_putchar(result % 10 + '0');
-	}
+		result = j * k;
+		if (result < 10)
+		{
+		if (k != 0)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar(result + '0');
+		}
+		else if (result < 100)
+		{
+		if (k != 0)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar(result / 10 + '0');
+		_putchar(result % 10 + '0');
+		}
+		else
+		{
+		_putchar(' ');
+		_putchar(result / 100 + '0');
+		remainder = result % 100;
+		_putchar(remainder / 10 + '0');
+		_putchar(remainder % 10 + '0');
+		}
+		if (n != k)
+		{
+		_putchar(',');
+		}
 	}
 	_putchar('\n');
 	}
